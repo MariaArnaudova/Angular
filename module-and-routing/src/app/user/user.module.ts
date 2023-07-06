5dd2f731efce932a290e4037f7ec9df92736aab9
MariaCode <m.p.arnaudova@gmail.com>
2023-07-06 14:48:41 +0300

    module-and-routing

---
+++ module-and-routing/src/app/user/user.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserListComponent } from './user-list/user-list.component';
+import { SharedModule } from '../shared/shared.module';
+import { RouterModule } from '@angular/router';
+import { UserDetailComponent } from './user-detail/user-detail.component';
+import { UserResolver } from './user-detail/user-detail.resolver';
+import { AuthGuard } from './user-detail.guard';
+
+
+
+@NgModule({
+  declarations: [
+    UserListComponent,
+    UserDetailComponent
+  ],
+  imports: [
+    CommonModule, SharedModule,
+    RouterModule.forChild([
+      {
+        path: 'user/list',
+        component: UserListComponent,
+      },
+      {
+        path: 'user/detail/:id',
+        resolve: {user: UserResolver},
+        canActivate: [AuthGuard],
+        component: UserDetailComponent,
+      }
+    ]),
+  ],
+  exports: [UserListComponent]
+})
+export class UserModule { }
---
+++ module-and-routing/src/app/user/user.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, OnInit } from '@angular/core';
+import { User } from './types/User';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService implements OnInit {
+
+  constructor(
+    private http: HttpClient) { }
+
+  ngOnInit(): void {
+    //
+  }
+
+  fetchUsers() {
+    return this.http.get<User[]>('http://jsonplaceholder.typicode.com/users');
+  }
+
+  fetchSingleUser(id: number) {
+    return this.http.get<User>(`http://jsonplaceholder.typicode.com/users/:${id}`);
+  }
+}
